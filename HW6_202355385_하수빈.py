cf41041408c3c8612bb87eadffe5773a05eaf959
anniex2100 <163281214+anniex2100@users.noreply.github.com>
2024-04-17 20:08:44 +0900

    HW6_202355385_하수빈

---
+++ HW6_202355385_하수빈.py
@@ -0,0 +1,31 @@
+## 1번
+
+# 출력 함수: print(), write(), writeline()
+# 입력 함수: input(), read(), readline(), readlines()
+
+
+
+## 2번
+
+# 1 (A - C - B - D)
+
+
+
+## 3번
+
+# 3 (a는 쓰기모드이다. 기존 파일 있으면 삭제하고 새로 만든다.)
+
+
+
+## 4번
+
+# 1 - open
+# 2 - readline
+# 3 - close
+
+
+
+## 6번
+
+# 1 - inFp.readlines()
+# 2 - outFp.write(inStr)
