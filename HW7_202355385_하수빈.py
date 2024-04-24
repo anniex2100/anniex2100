ef7e40df265b4bd78d863316add52adebf8a303c
anniex2100 <163281214+anniex2100@users.noreply.github.com>
2024-04-24 21:07:08 +0800

    HW7_202355385_하수빈

---
+++ HW7_202355385_하수빈.py
@@ -0,0 +1,19 @@
+## 2번
+class Car:
+    color = ""
+    speed = 0
+
+    def upSpeed(self,value):
+        self.speed += value
+
+## 4번
+class Horse:
+    speed = 0
+    def__init__(self):
+        self.speed = 50
+
+## 6번
+#정답: 3번
+
+## 7번
+#정답: class RVCar(Car)
